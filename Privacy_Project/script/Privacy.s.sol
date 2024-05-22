aab23a19aabc2cf6062c8b147310ad519a6ed757
Malthus Ronaldo Djatche <djatchemalthus@gmail.com>
2024-05-22 17:22:06 +0200

    Thirth commit

--- Privacy_Project/script/Privacy.s.sol
+++ Privacy_Project/script/Privacy.s.sol
@@ -7,16 +7,19 @@ contract PrivacySolution is Script {
     
     function run() public {
 
+      // Ici j'initialise un tableau bytes32 avec des données fictives
         bytes32[3] memory data = [
             bytes32("one"),
             bytes32("two"),
             bytes32("three")
         ];
 
+        //Je déploie le contrat Privacy avec ces données.
         Privacy privacy = new Privacy(data);
 
         console.log("Locked:", privacy.locked());
 
+    // J'extrait la clé nécessaire pour déverrouiller le contrat à partir de data[2].
         bytes16 key = bytes16(data[2]);
 
         privacy.unlock(key);
