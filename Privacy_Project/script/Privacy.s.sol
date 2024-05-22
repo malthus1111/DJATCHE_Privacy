01093bf5664c12a7cd81536cf583f68a781c6d35
Malthus Ronaldo Djatche <djatchemalthus@gmail.com>
2024-05-22 17:18:13 +0200

    Second commit

--- Privacy_Project/script/Privacy.s.sol
+++ Privacy_Project/script/Privacy.s.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import {Script, console} from "forge-std/Script.sol";
+import "../src/Privacy.sol";
 
 contract PrivacySolution is Script {
     
+    function run() public {
+
+        bytes32[3] memory data = [
+            bytes32("one"),
+            bytes32("two"),
+            bytes32("three")
+        ];
+
+        Privacy privacy = new Privacy(data);
+
+        console.log("Locked:", privacy.locked());
+
+        bytes16 key = bytes16(data[2]);
+
+        privacy.unlock(key);
+
+        console.log("Locked:", privacy.locked());
+    }
     
 }
